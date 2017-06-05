e1460d80e30b9004cb3da48dab57e70df83b9d44
Xinyang <xinyzhang9@gmail.com>
2017-06-05 16:26:32 -0700

    fork

---
+++ main.js
@@ -0,0 +1,36 @@
+
+var countItems = $(".item").length,
+    random , 
+    getIconName , 
+    getColor,
+    deg = [];
+
+for(var x = 0 ; x < countItems ; x++){
+    $(".item ").eq(x).css("transform" , "rotate(" + x * 360 / countItems +"deg)");
+}
+for(var i = 360 ; i <= (360*9) ; i += 360){
+  deg.push(i) ;
+};
+
+function randVal(num){
+  return Math.floor(Math.random() * num );
+}
+
+$(".spin").on("click",function(){
+  random = randVal(countItems);
+  randDeg = deg[randVal(deg.length)] ;
+  getColor = $('.item').eq(random).css("borderTopColor");
+  getIconName = $('.item').eq(random).find("span").attr("data-name");
+
+
+(random == 5 ) ?  randDeg += 60 :'';
+(random == 4) ?  randDeg += 120 :'';
+(random == 3 ) ?  randDeg += 180 :'';
+(random == 2 ) ?  randDeg += 240 :'';
+(random == 1 ) ?  randDeg += 300 :'';
+
+  $(".inner").css({transform : "rotate(" + randDeg  + "deg)"});
+  
+  $(".title , body").css({backgroundColor: getColor , color: "#FFF" });
+  $(".title h3").text(getIconName);
+});
